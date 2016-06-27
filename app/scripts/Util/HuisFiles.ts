8474d9d07101f6e3d9b8704c90da90dd01c77a5a
TE-MotokiSasaki <Motoki.Sasaki@jp.sony.com>
2016-06-27 17:39:55 +0900

    add font weight element to function wring module file to huis

--- app/scripts/Util/HuisFiles.ts
+++ app/scripts/Util/HuisFiles.ts
@@ -729,7 +729,7 @@ module Garage {
 			/**
 			 * Image データから module 化に不要な物を間引く
 			 */
-			private _normalizeLabels(labels: ILabel[]): ILabel[] {
+			private _normalizeLabels(labels: IGLabel[]): ILabel[] {
 				var normalizedLabels: ILabel[] = [];
 
 				for (let i = 0, l = labels.length; i < l; i++) {
@@ -746,7 +746,12 @@ module Garage {
 					}
 					if (label.size !== undefined) {
 						normalizedLabel.size = label.size;
-					}
+                    }
+                    if (label.font_weight !== undefined) {
+                        normalizedLabel.font_weight = label.font_weight;
+                    }
+
+                    //fontWeightをFontWeight >> stringに
 					normalizedLabels.push(normalizedLabel);
 				}
 
