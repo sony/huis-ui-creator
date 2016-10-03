f63ecce814906d9f30fc85c4b963c084c365dc95
TE-MotokiSasaki <Motoki.Sasaki@jp.sony.com>
2016-10-03 15:22:41 +0900

    change default text for text button

--- app/scripts/View/FullCustom.ts
+++ app/scripts/View/FullCustom.ts
@@ -2025,6 +2025,11 @@ module Garage {
 				let $textField: JQuery = $(".property-state-text-value[data-state-id=\"" + stateId + "\"]");
 
 				let textInTextFiled: string = $textField.val();
+                
+                if (textInTextFiled == null || textInTextFiled == "") {
+                    textInTextFiled = $.i18n.t("button.text_button.STR_REMOTE_BTN_TEXT_BTN_DEFAULT");
+                    $textField.val(textInTextFiled);
+                }
 
 				this._updateCurrentModelStateData(TARGET_ALL_STATE,
 					{
