6da8dafc21255d25440e13c8dd02f73b39fa7612
TE-MotokiSasaki <Motoki.Sasaki@jp.sony.com>
2016-10-03 20:59:04 +0900

    fix bug about text label and button size sometime changed

--- app/scripts/View/ButtonItem.ts
+++ app/scripts/View/ButtonItem.ts
@@ -4,7 +4,8 @@
 
 module Garage {
 	export module View {
-		import Tools = CDP.Tools;
+        import Tools = CDP.Tools;
+        import JQUtils = Util.JQueryUtils;
 		var TAG = "[Garage.View.ButtonItem] ";
 
 		export class ButtonItem extends Backbone.View<Model.ButtonItem> {
@@ -101,11 +102,11 @@ module Garage {
                         }
                     }
 
-					//表示用のmodelはラベルの大きさを実際より小さくする。減衰率はRATIO_TEXT_SIZE_HUIS_GARAGE_BUTTON
+					//表示用のmodelはラベルの大きさを実際より小さくする。
 					let modelForDisplay: Model.ButtonItem= jQuery.extend(true, {}, model);
 					for (let i = 0; i < modelForDisplay.state.length; i++){
 						for (let j = 0; j < modelForDisplay.state[i].label.length; j++){
-							modelForDisplay.state[i].label[j].size = Math.round(modelForDisplay.state[i].label[j].size * RATIO_TEXT_SIZE_HUIS_GARAGE_BUTTON);
+                            modelForDisplay.state[i].label[j].size = JQUtils.getOffsetTextButtonSize(modelForDisplay.state[i].label[j].size);
 						}
 					}
 					this.$el.append($(this.buttonItemTemplate_(modelForDisplay)));
@@ -133,11 +134,11 @@ module Garage {
 			 */
 			private _renderNewModel(model: Model.ButtonItem) {
 				this._modifyModel(model);
-				//表示用のmodelはラベルの大きさを実際より小さくする。減衰率はRATIO_TEXT_SIZE_HUIS_GARAGE_BUTTON
+				//表示用のmodelはラベルの大きさを実際より小さくする。
 				let modelForDisplay: Model.ButtonItem = jQuery.extend(true, {}, model);
 				for (let i = 0; i < modelForDisplay.state.length; i++) {
 					for (let j = 0; j < modelForDisplay.state[i].label.length; j++) {
-						modelForDisplay.state[i].label[j].size = Math.round(modelForDisplay.state[i].label[j].size * RATIO_TEXT_SIZE_HUIS_GARAGE_BUTTON);
+                        modelForDisplay.state[i].label[j].size = JQUtils.getOffsetTextButtonSize(modelForDisplay.state[i].label[j].size); 
 					}
 				}
 				this.$el.append($(this.buttonItemTemplate_(modelForDisplay)));
