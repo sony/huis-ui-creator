dcf0b9abfd1eb8ea58b76547690adc910d3a84df
TE-MotokiSasaki <Motoki.Sasaki@jp.sony.com>
2016-10-06 11:52:05 +0900

    fix bug, delete image of button when become text button

--- app/scripts/View/FullCustom.ts
+++ app/scripts/View/FullCustom.ts
@@ -3479,12 +3479,16 @@ module Garage {
 										width: buttonAreaW + "px",
 										height: buttonAreaH + "px",
                                     });
-                                    
 
-                                    let inputUrl: string = this.getValidPathOfIGImageForCSS(targetState.image[0]);
+                                    let inputUrl: string = null;
+                                    if (targetState.image!= null && targetState.image[0] != null) {
+                                        inputUrl = this.getValidPathOfIGImageForCSS(targetState.image[0]);
+                                    }
+
                                     if (inputUrl == null) {
                                         inputUrl = "none";
                                     }    
+
                                     this.setBackgroundImageUrlInCSS($imageElement, inputUrl);
 
 
