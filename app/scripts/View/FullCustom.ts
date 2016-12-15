a44dc6b5c5fc0f2a2d82a5ce28fd9f2d6d4531a9
Yuuki Todoroki <Yuuki.Todoroki@jp.sony.com>
2016-12-15 15:58:50 +0900

    [Remove] Remove textbox focus in updating size of Item

--- app/scripts/View/FullCustom.ts
+++ app/scripts/View/FullCustom.ts
@@ -3653,15 +3653,6 @@ module Garage {
 
                                     //画像が存在するとき、テキストEdit機能を非表示にする
                                     this.toggleImagePreview(stateId);
-
-                                    //テキストエリアが表示されたとき、フォーカスを移す。
-                                    var $textFieldInPreview = $(".property-state-text-value[data-state-id=\"" + stateId + "\"]");
-                                    if ($textFieldInPreview.css("visibility") === "visible") {
-                                        setTimeout(function () {
-                                            $textFieldInPreview.focus();
-                                        }, 0);
-                                    }
-
                                 }
                                 break;
 
