474b0e618231400375e17fc9646ccf3325ff83ad
Yuuki Todoroki <Yuuki.Todoroki@jp.sony.com>
2016-12-19 20:52:28 +0900

    [Add] Add _cloneFace method

--- app/scripts/View/Home.ts
+++ app/scripts/View/Home.ts
@@ -261,6 +261,15 @@ module Garage {
             //}
 
            /**
+             * faceのcloneを作成する。型情報はコピーされない事に注意。
+             *
+             * @param face {IGFace} コピーしたいface。
+             */
+            private _cloneFace(face: IGFace) {
+                return $.extend(true, {}, face);
+            }
+
+           /**
              * 引数で与えたremoteIdを持つリモコンの編集画面に移動する。
              *
              * @param remoteId {string} 0埋め4桁の数字文字列。省略した場合は、新規リモコン作成画面に入る。
