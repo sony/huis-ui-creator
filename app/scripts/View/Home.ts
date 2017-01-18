313b73737e72fb5f75e7fcb896f07962fa6d241b
Yuuki Todoroki <Yuuki.Todoroki@jp.sony.com>
2017-01-18 20:08:31 +0900

    [Modify] Move createNewRemote button to face list

--- app/scripts/View/Home.ts
+++ app/scripts/View/Home.ts
@@ -68,7 +68,6 @@ module Garage {
                 return $.extend(ret,{
                     //"dblclick header .ui-title": "_onHeaderDblClick",
                     "click #create-new-remote": "_onCreateNewRemote",
-                    "mouseover #create-new-remote": "_onCreateNewRemoteHover",
                     "click #sync-pc-to-huis": "_onSyncPcToHuisClick",
                     "click #option-pulldown-menu": "_onOptionPullDownMenuClick",
                     "click #command-import-remote": "onOptionImport",
@@ -179,9 +178,7 @@ module Garage {
                 }
 
                 //テキストのローカライズ
-                $("header h3").html($.i18n.t("home.STR_HOME_TITLE"));
-                $("#create-new-remote").attr("title", $.i18n.t("tooltip.STR_TOOLTIP_NEW_REMOTE"));
-                
+                $("#page-home").i18n();
             }
 
 
