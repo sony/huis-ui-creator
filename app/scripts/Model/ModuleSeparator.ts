a14e3bfb8ca9313106ca70d8ed2fbc053f568c88
Yuuki Todoroki <Yuuki.Todoroki@jp.sony.com>
2017-01-20 11:35:39 +0900

    [Fix] Fix font-size and font-weight of ModuleSeparator

--- app/scripts/Model/ModuleSeparator.ts
+++ app/scripts/Model/ModuleSeparator.ts
@@ -6,6 +6,9 @@ module Garage {
 
         const HORIZONTAL_LINE_IMAGE_PATH: string = "/res/images/divider_pickup_custom.png";
 
+        const MODULE_SEPARATOR_LABEL_FONT_SIZE = 18;
+        const MODULE_SEPARATOR_LABEL_FONT_WEIGHT = "normal";
+
         export class ModuleSeparator extends Backbone.Model {
 
             constructor(text: string, attributes?: any) {
@@ -75,8 +78,8 @@ module Garage {
             private itemizeLabel(): Model.LabelItem {
                 let newLabel = new Model.LabelItem();
                 let $moduleSeparator = $(".module-separator");
-                newLabel.size = parseInt($moduleSeparator.css("font-size"), 10);
-                newLabel.font_weight = $moduleSeparator.css("font_weight");
+                newLabel.size = MODULE_SEPARATOR_LABEL_FONT_SIZE;
+                newLabel.font_weight = MODULE_SEPARATOR_LABEL_FONT_WEIGHT;
 
                 newLabel.area.x = BIAS_X_DEFAULT_GRID_LEFT;
                 newLabel.area.y = 0;
