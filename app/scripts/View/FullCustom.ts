df8299d907d2837ba5a18d764d33af3f20f3e7d9
Yuuki Todoroki <Yuuki.Todoroki@jp.sony.com>
2017-01-30 14:59:51 +0900

    [Fix] Add ctrl modifier key in copy/paste

--- app/scripts/View/FullCustom.ts
+++ app/scripts/View/FullCustom.ts
@@ -5782,10 +5782,14 @@ module Garage {
                             this._deleteCurrentTargetItem();
                             break;
                         case 67: // c Copy
-                            this.setClipboadToItem();
+                            if (event.ctrlKey) {
+                                this.setClipboadToItem();
+                            }
                             break;
                         case 86: // v Paste
-                            this.pasteItemFromClipboard();
+                            if (event.ctrlKey) {
+                                this.pasteItemFromClipboard();
+                            }
                             break;
                         case 90: // z Undo
                             if (event.ctrlKey) {
