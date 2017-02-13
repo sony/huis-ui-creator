67b1b50a7f4c178fbc1efa75b5aadad761d31250
Yuuki Todoroki <Yuuki.Todoroki@jp.sony.com>
2017-02-13 14:15:20 +0900

    [Change] Set default encode of css

--- app/stylesheets/config.rb
+++ app/stylesheets/config.rb
@@ -1,5 +1,7 @@
 # Require any additional compass plugins here.
 
+Encoding.default_external = 'utf-8'
+
 # Set this to the root of your project when deployed:
 http_path = "../"
 css_dir = "../stylesheets"
