22644875785e719af9b93c350367d8d673c093c9
Yuuki Todoroki <Yuuki.Todoroki@jp.sony.com>
2017-02-14 19:00:39 +0900

    [Change] Replace fs.rmdir with fs.remove

--- app/scripts/Util/HuisDev.ts
+++ app/scripts/Util/HuisDev.ts
@@ -395,8 +395,8 @@
                             this._removeFiles(destRootDir, removeTargetFiles)
                                 .then(() => {
                                     df.resolve();
-                                    fs.rmdir(emptyDirectory);
-                                    fs.rmdirSync(targetDirectoryPath);
+                                    fs.remove(emptyDirectory);
+                                    fs.removeSync(targetDirectoryPath);
 
                                     if (callback) {
                                         callback(null);
@@ -553,8 +553,8 @@
                                     let fileStat = fs.lstatSync(filePath);
                                     if (fileStat) {
                                         if (fileStat.isDirectory()) {
-                                            console.log("rmdirSync: " + file);
-                                            fs.rmdirSync(filePath);
+                                            console.log("remove: " + file);
+                                            fs.removeSync(filePath);
                                         } else {
                                             console.log("unlinkSync: " + file);
                                             fs.unlinkSync(filePath);
