bd5b1c3880a40c0ab3cb1903a02ef19ca93091a7
TE-MotokiSasaki <Motoki.Sasaki@jp.sony.com>
2017-05-09 20:35:54 +0900

    swich show or not show, jump button

--- app/scripts/Util/HuisFiles.ts
+++ app/scripts/Util/HuisFiles.ts
@@ -120,7 +120,15 @@ module Garage {
                 if (!this.commonRemoteInfo_) {
                     console.log("setting commonRemoteInfo_");
                     let remoteId = "common";
+
                     let facePath = Util.MiscUtil.getAppropriatePath(CDP.Framework.toUrl("/res/faces/common/common.face"));
+
+                    //ビジネス仕向けか否かで、表示するCommonアイテムを変える。
+                    if (Util.MiscUtil.isBz()) {
+                        facePath = Util.MiscUtil.getAppropriatePath(CDP.Framework.toUrl("/res/faces/common/common_bz.face"));
+                    } else {
+                        facePath = Util.MiscUtil.getAppropriatePath(CDP.Framework.toUrl("/res/faces/common/common.face"));
+                    }
                     console.log("facePath=" + facePath);
 
                     //// file:/// スキームがついていると fs モジュールが正常に動作しないため、file:/// がついていたら外す
