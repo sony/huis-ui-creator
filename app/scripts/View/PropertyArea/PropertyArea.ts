fd2d32dc5ea2fe9b9c37e3c10855937cc80a96ab
TE-MotokiSasaki <Motoki.Sasaki@jp.sony.com>
2017-06-07 12:51:00 +0900

    fix comment about arguments

--- app/scripts/View/PropertyArea/PropertyArea.ts
+++ app/scripts/View/PropertyArea/PropertyArea.ts
@@ -76,8 +76,8 @@ module Garage {
              * CommandManagerにModelの変更を登録する。
              * PropertyArea上の変更はこの関数での変更のみとする。
              * @param {Model.Item} target 変更対象のモデル。
-             * @param {Object} prevValue 変更前の値。undo時に利用。
-             * @param {Object} nextValue 変更後の愛。redo時に利用。
+             * @param {Object} previousData 変更前の値。undo時に利用。
+             * @param {Object} nextData 変更後の愛。redo時に利用。
              */
             protected _setMementoCommand(target: Model.Item, previousData: Object, nextData: Object) {
                 let FUNCTION_NAME = TAG + "_setMementoCommand ";
