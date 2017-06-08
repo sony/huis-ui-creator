076289d7d70b2d95417c2009b0e2b775c6f2ee73
TE-MotokiSasaki <Motoki.Sasaki@jp.sony.com>
2017-06-08 16:54:36 +0900

    add comment

--- app/scripts/View/PropertyArea/PreviewWindow/ImageHandlePreviewWindow.ts
+++ app/scripts/View/PropertyArea/PreviewWindow/ImageHandlePreviewWindow.ts
@@ -157,7 +157,8 @@ module Garage {
                 let imageName = path.basename(imageFilePath);
                 let dirPath = this.getModel().getNotDefaultImageDirFullPath();
                 let outputImagePath = path.join(dirPath, imageName).replace(/\\/g, "/");
-
+                
+                //TODO: move const variables difinition from init.ts to more specific place
                 let params = this.getModel().isBackgroundImage() ? IMAGE_EDIT_PAGE_BACKGROUND_PARAMS : IMAGE_EDIT_PARAMS;
 
                 Model.OffscreenEditor.editImage(imageFilePath, params, outputImagePath)
