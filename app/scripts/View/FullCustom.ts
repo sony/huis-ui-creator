250c7103811af726e672947a4b7ce5464530fade
TE-MotokiSasaki <Motoki.Sasaki@jp.sony.com>
2017-06-12 20:32:37 +0900

    fix bug layout break of text field

--- app/scripts/View/FullCustom.ts
+++ app/scripts/View/FullCustom.ts
@@ -3704,7 +3704,7 @@ module Garage {
                     this.listenTo(item, "change", this._updateElementsOnCanvasProperyAreaChanged);
                 }
                 $detail.append(this.propertyArea_.render().$el);
-                $detail.trigger("create");
+                $detail.find(".custom-select").trigger("create");
 
                 if (this.propertyArea_ == null) {
                     console.warn(TAG + "_showDetailItemArea() unknown type item");
