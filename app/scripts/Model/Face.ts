e923b9b37c6e913c1b12d9ba157ab4f5191ba301
TE-MotokiSasaki <Motoki.Sasaki@jp.sony.com>
2017-06-28 16:46:08 +0900

    care module.button is null case

--- app/scripts/Model/Face.ts
+++ app/scripts/Model/Face.ts
@@ -333,6 +333,9 @@ module Garage {
                     if (module.image != null) {
                         images = images.concat(module.image);
                     }
+                    if (module.button == null) {
+                        continue;
+                    }
                     for (let button of module.button) {
                         for (let state of button.state) {
                             if (state.image != null) {
