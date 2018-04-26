2a7bbd5c7311606cc57a301025236798adb3fb8c
Kosuke Mita <KosukeMita@users.noreply.github.com>
2018-04-26 14:03:33 +0900

    [add] add Model.ScreensaverDialog

---
+++ app/scripts/Model/Dialog/ScreenSaverDialog.ts
@@ -0,0 +1,36 @@
+﻿/*
+    Copyright 2016 Sony Corporation
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/// <reference path="../../include/interfaces.d.ts" />
+
+
+module Garage {
+    export module Model {
+        var TAG = "[Garage.Model.VersionDialog] ";
+
+        export class ScreensaverDialog extends Backbone.Model {
+            private _image_path: string;
+
+            get image_path(): string {
+                return this._image_path;
+            }
+
+            set image_path(path: string) {
+                this._image_path = path;
+            }
+        }
+    }
+}
